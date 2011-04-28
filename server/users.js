0bef68a420bdd94b89aa83fc9ba8f1ff7d147280
Felipe Trzaskowski <666.felipe@gmail.com>
2011-04-28 02:15:11 -0300

    Mais alguns ajustes na criação de usuários.

--- server/users.js
+++ server/users.js
@@ -3,20 +3,30 @@ var redis = require('redis').createClient();
 exports.create = create;
 exports.authenticate = authenticate;
 
-
 /* Public functions */
 
 function create(user_data, answer) {
-    if (user_data.name &&
-	user_data.login &&
-	user_data.password &&
-	user_data.email &&
-	user_data.birth_date) {
-	redis.hsetnx('users', user_data.login, JSON.stringify(user_data), function(err, ret) { after_create(ret, answer); });
+    if (user_data.name && user_data.login && user_data.password && user_data.email && user_data.birth_date) {
+
+	db_entry = {
+	    'name': user_data.name,
+	    'password': user_data.password,
+	    'email': user_data.email,
+	    'birth_date': user_data.birth_date
+	}
+
+	redis.hsetnx('users', user_data.login, JSON.stringify(db_entry), function(err, ret) { after_create(ret, answer); });
+
     } else {
 	answer(400, 'Dados Incompletos.');
     }
 }
+
+function authenticate(user_data, answer) {
+    redis.hget('users', user_data.login, function(err, ret) { auth_after_get(user_data, ret, answer); });
+}
+
+/* Private functions */
 
 function after_create(ret, answer) {
     if (ret == 1) {
@@ -26,12 +36,9 @@ function after_create(ret, answer) {
     }
 }
 
-function authenticate(user_data, answer) {
-    redis.hget('users', user_data.login, function(err, ret) { auth_after_get(user_data, ret, answer); });
-}
-
 function auth_after_get(data, ret, answer) {
     if (ret) {
+
 	ret_data = JSON.parse(ret);
 
 	if (data.password == ret_data.password) {
@@ -39,6 +46,7 @@ function auth_after_get(data, ret, answer) {
 	} else {
 	    answer(401, 'Senha Invalida.');
 	}
+
     } else {
 	answer(401, 'Usuario Invalido.');
     }
